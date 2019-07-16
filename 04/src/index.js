bd0787ae5abac562ed81ef0fdeb09bf1d666cdb0
zhangnuli <492888231@qq.conm>
2019-07-16 13:25:38 +0800

    开发环境和html插件

---
+++ 04/src/index.js
@@ -0,0 +1,13 @@
+// console.log('webpack')
+
+//CommonJS方式
+// const sum = require('./common/common.js')
+// console.log(sum(1, 2))
+
+//es6方式
+// import sum1 from './common/es6'
+// console.log(sum1(1, 3))
+
+//引入css文件
+
+// import './css/index.css'
---
+++ 04/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+module.exports = {
+  mode: 'development',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    open:true,
+    progress :true
+  },
+  entry: './src/index.js',
+  output: {
+    filename: 'build.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      title:'测试',
+      minify:false,
+      template:path.resolve(__dirname,'src/index.html'),
+      filename:'test.html'
+    })
+  ]
+}
