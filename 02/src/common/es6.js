4296dcaa1b9312f9a51a58f9daf243b401c6072e
zhangnuli <492888231@qq.conm>
2019-07-16 10:10:08 +0800

    配置文件

---
+++ 02/src/common/es6.js
@@ -0,0 +1,5 @@
+function sum1(a, b) {
+    return a + b
+}
+
+export default sum1
---
+++ 02/webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path')
+module.exports = {
+    mode:'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'build.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+}
