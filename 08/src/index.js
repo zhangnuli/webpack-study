bcb0f47508988e391a3c6fbe77d6a792771ea68b
zhangnuli <492888231@qq.conm>
2019-07-17 10:07:29 +0800

    使用eslint校验js语法

---
+++ 08/src/index.js
@@ -0,0 +1,11 @@
+// import './css/index.css'
+
+let my = ()=>{
+    console.log('my')
+}
+
+class Toq{
+    init(){
+        console.log('init');
+    }
+}
---
+++ 08/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+module.exports = {
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin(),
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    open: true,
+    progress: true
+  },
+  entry: {
+    index: './src/index.js'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'eslint-loader',
+        }
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+
+    ]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      title: '测试',
+      minify: false,
+      template: path.resolve(__dirname, 'src/index.html'),
+      chunks: ['index']
+    })
+  ]
+}
